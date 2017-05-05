118d805a5bdcc5af8853d766f85028486989f8af
Infologs <webboostings@gmail.com>
2017-05-05 16:44:07 +0530

    How to use filters in AngularJS

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+﻿var app = angular.module("app01", []);
+
+app.controller("controller1",["$scope",function($scope){
+	$scope.Name = "Abhishek";
+	$scope.CountryName = "";
+	$scope.Amount = "200";
+}]);
