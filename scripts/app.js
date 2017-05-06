5779dad4c6e933f2389157183a09c80953f08d3d
Infologs <webboostings@gmail.com>
2017-05-06 16:16:11 +0530

    Added some more examples

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,22 @@ app.controller("controller1",["$scope",function($scope){
 	$scope.Name = "Abhishek";
 	$scope.CountryName = "";
 	$scope.Amount = "200";
+	$scope.SortType = "name";
+
+	$scope.Information = [
+		{name:'Jani',country:'Norway'},
+		{name:'Abhishek',country:'India'},
+		{name:'Ravi',country:'India'},
+		{name:'Scott',country:'US'},
+		{name:'Tiger',country:'US'},
+		{name:'David',country:'Australia'},
+		{name:'Sri',country:'Newzeland'},
+		{name:'Harsha',country:'England'},
+		{name:'Cameron',country:'SriLanka'},
+		{name:'Paresh',country:'Brazil'}
+		]
+
+	$scope.Sort = function(type) {
+		$scope.SortType = type;
+	}
 }]);
